c22d6a24e69598e8b0ac0cccfa4d383c67ac94ab
Mirko Sandoval <mirkoxd.314@gmail.com>
2025-08-27 11:51:19 -0400

    Agrega lógica de calculadora - MainActivity4.kt/activity_main4.xml

--- ProyectoBase/app/src/main/java/com/example/proyectobase/MainActivity4.kt
+++ ProyectoBase/app/src/main/java/com/example/proyectobase/MainActivity4.kt
@@ -1,14 +1,16 @@
 package com.example.proyectobase
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import com.example.proyectobase.utils.*
 class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +21,45 @@ class MainActivity4 : AppCompatActivity() {
         val numeroDos: EditText = findViewById(R.id.ed_opDos)
         val txResultado: TextView = findViewById(R.id.tx_resultado_act4)
         val btnCalcular: Button = findViewById(R.id.btn_calcular)
+        val spOperaciones : Spinner = findViewById(R.id.sp_operaciones)
+
+        //creacion del primer array
+        val menuSPoperaciones = listOf("Sumar", "Restar", "Multiplicar", "Dividir")
+
+        //vamos a completar el spinner de operaciones
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            menuSPoperaciones
+        )
+
+        spOperaciones.adapter = adapter
+
+        btnCalcular.setOnClickListener {
+            var calcN1: Int = numeroUno.text.toString().toIntOrNull() ?: 0
+            var calcN2: Int = numeroDos.text.toString().toIntOrNull() ?: 0
+
+            if(spOperaciones.selectedItem =="Sumar"){
+                var resultadoOP: Int = OpMatematicas.sumar(calcN1, calcN2)
+                txResultado.text = resultadoOP.toString()
+
+            }else if(spOperaciones.selectedItem =="Restar"){
+                var resultadoOP: Int = OpMatematicas.restar(calcN1, calcN2)
+                txResultado.text = resultadoOP.toString()
+
+            }else if(spOperaciones.selectedItem =="Multiplicar"){
+                var resultadoOP: Int = OpMatematicas.multiplicar(calcN1, calcN2)
+                txResultado.text = resultadoOP.toString()
+            }else if(spOperaciones.selectedItem =="Dividir"){
+                var resultadoOP: Int = OpMatematicas.dividir(calcN1, calcN2)
+                txResultado.text = resultadoOP.toString()
+            }else{
+                null
+            }
 
 
+
+        }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
---
+++ ProyectoBase/app/src/main/java/com/example/proyectobase/utils/OpMatematicas.kt
@@ -0,0 +1,22 @@
+package com.example.proyectobase.utils
+
+object OpMatematicas {
+
+    /** Funcion sumar recibe 2 parametros, retorna un número entero.**/
+
+    fun sumar(n1: Int, n2: Int): Int{
+        return n1 + n2
+    }
+
+    fun restar(n1: Int, n2: Int): Int{
+        return n1 - n2
+    }
+
+    fun multiplicar(n1: Int, n2: Int): Int{
+        return n1 * n2
+    }
+
+    fun dividir(n1: Int, n2: Int): Int{
+        return n1 / n2
+    }
+}
